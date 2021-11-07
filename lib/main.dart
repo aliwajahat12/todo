59c94dc94ac56f96df5104f395d587fabe34bb71
Wajahat Ali <k180363@nu.edu.pk>
2021-11-07 16:52:02 +0500

    implemented

--- lib/main.dart
+++ lib/main.dart
@@ -28,38 +28,80 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late TextEditingController taskController;
 
-  void _incrementCounter() {
+  List<String> task = [];
+
+  @override
+  void initState() {
+    taskController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    taskController.dispose();
+    super.dispose();
+  }
+
+  void addTaskToList() {
     setState(() {
-      _counter++;
+      final todoTask = taskController.text;
+      taskController.clear();
+      task.add(todoTask);
     });
+  }
+
+  void deleteTask(String taskString) {
+    setState(() {
+      task.removeWhere((element) => element == taskString);
+    });
+  }
+
+  Widget taskDisplay(String todoString) {
+    return ListTile(
+      leading: const Icon(Icons.ac_unit),
+      title: Text(todoString),
+      trailing: IconButton(
+          onPressed: () => deleteTask(todoString),
+          icon: const Icon(Icons.delete)),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('TODO App'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: taskController,
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: addTaskToList, icon: const Icon(Icons.add))
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: task.length,
+                  itemBuilder: ((context, i) {
+                    return taskDisplay(task[i]);
+                  }),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
